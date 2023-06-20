dcfe8a04468d92fc6b92e92c2ad75d9131fd3b7f
Aleksandr Lovygin <Shurik.lovygin@gmail.com>
2023-06-20 13:11:16 +0300

    problem solved using recursion

---
+++ Task_068/Program.cs
@@ -0,0 +1,22 @@
+﻿//Write a program to calculate the Ackermann function using recursion
+
+using System;
+using static System.Console;
+
+Clear();
+WriteLine("Please, enter decimal number m");// to mark the beginning - чтобы обозначить начало
+int m = int.Parse(ReadLine());
+
+WriteLine("Please, enter decimal number n");// to mark the end - чтобы обозначить конец
+int n = int.Parse(ReadLine());
+WriteLine();
+
+int Ackermann(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if (n == 0) return Ackermann(m-1,1);
+    else return Ackermann(m-1, Ackermann(m, n -1));
+}
+
+int res = Ackermann(m,n);
+WriteLine(res);
