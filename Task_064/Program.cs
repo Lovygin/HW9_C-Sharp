3c40df6d87820b40af8c83d313710402ed88390d
Aleksandr Lovygin <Shurik.lovygin@gmail.com>
2023-06-20 11:02:58 +0300

    problem solved using recursion

---
+++ Task_064/Program.cs
@@ -0,0 +1,16 @@
+﻿//Ounput value between N and 1 using recursion.
+using System;
+using static System.Console;
+
+Clear();
+WriteLine("Please, enter decimal number for the output on the screen");
+int number = int.Parse(ReadLine());
+
+OutputNumbers(number);
+
+void OutputNumbers(int number)
+{
+    Write(number + "\t");
+    if(number == 1) return;
+    else number--; OutputNumbers(number);
+}
